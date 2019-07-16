92f266958b0b5f1b45dfe87bfd7d49904d880a01
chenggongcai <1197063758@qq.com>
2019-07-16 20:53:52 +0800

    完成Vue项目在脚手架2的版本下的一些静态数据显示

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <router-view></router-view>
+  <FooterGuide v-show="$route.meta.isShowFooter"/>
+</div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide'
+// 引入api
+import { reqAddress, reqCategory, reqShops } from '../src/api'
+export default {
+  name: 'App',
+  // 注册组件
+  components: {
+    FooterGuide
+  },
+  async mounted () {
+    const result1 = await reqAddress("116.36867", "40.10038")
+    console.log(result1);
+    const result2 = await reqCategory()
+    console.log(result2);
+    const result3 = await reqShops("116.36867", "40.10038")
+    console.log(result3);
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+// 注册路由器
+import router from './router'
+// 定义全局组件
+import Header from './components/Heard/index.vue'
+Vue.component('Header', Header)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  router
+})
